9db22c6c79d42d4d9c0cc9896019a5471f272505
SUDHARSAN S <sudharsa8850@gmail.com>
2025-07-26 10:19:44 +0530

    added time interval

--- src/Main.js
+++ src/Main.js
@@ -1,50 +1,74 @@
-import React, { useState } from "react";
-import Pic1 from "./assests/caroseul-1.jpg";
-import Pic2 from "./assests/caroseul-2.jpg";
-import Pic3 from "./assests/caroseul-3.jpg";
-import Pic4 from "./assests/caroseul-4.jpg";
+  import React, { useEffect, useState } from "react";
+  import Pic1 from "./assests/caroseul-1.jpg";
+  import Pic2 from "./assests/caroseul-2.jpg";
+  import Pic3 from "./assests/caroseul-3.jpg";
+  import Pic4 from "./assests/caroseul-4.jpg";
 
-function Main() {
-  const images = [Pic1, Pic2, Pic3, Pic4];
-  const [currentPic, setCurrentPic] = useState(0);
+  function Main() {
+    const images = [Pic1, Pic2, Pic3, Pic4];
+    const [currentPic, setCurrentPic] = useState(0);
+    const caroseuDialogue = [
+      {
+        smallText: "From pictures to moments",
+        bigText: "Turn your photos into art",
+      },
+      {
+        smallText: "Capture the vibe",
+        bigText: "Make every shot unforgettable",
+      },
+      {
+        smallText: "Every click tells a story",
+        bigText: "Your memories, reimagined",
+      },
+      {
+        smallText: "Frame your favorites",
+        bigText: "Art that speaks for you",
+      },
+    ];
 
-  const caroseul1 = {
-    backgroundImage: `url(${images[currentPic]})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    heigth: "100%",
-    width: "100%",
-    opacity: "",
-  };
+    useEffect(() => {
+      setInterval(() => {
+        currentPic === images.length - 1
+          ? setCurrentPic(0)
+          : setCurrentPic(currentPic + 1);
+      }, 5000);
+    }, []);
 
-  const caroseuDialogue = [{}];
+    const caroseul1 = {
+      backgroundImage: `url(${images[currentPic]})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      heigth: "100%",
+      width: "100%",
+      opacity: "",
+    };
 
-  const caroseulSlide = () => {
-    return images.map((_, index) => (
-      <span
-        key={index}
-        className={`point ${currentPic === index && "currentPic"}`}
-        onClick={() => {
-          setCurrentPic(index);
-        }}
-      ></span>
-    ));
-  };
+    const caroseulSlide = () => {
+      return images.map((_, index) => (
+        <span
+          key={index}
+          className={`point ${currentPic === index && "currentPic"}`}
+          onClick={() => {
+            setCurrentPic(index);
+          }}
+        ></span>
+      ));
+    };
 
-  return (
-    <div className="main">
-      <div className="caroseuls" style={caroseul1}>
-        <div className="caroseulText">
-          <h3 className="smallText">From pictures to moments</h3>
-          <h3 className="bigText">Turn your photos into art</h3>
-          <div className="btnSlide">
-            <button className="exploreBtn">Explore more</button>
-            <div>{caroseulSlide()}</div>
+    return (
+      <div className="main">
+        <div className="caroseuls" style={caroseul1}>
+          <div className="caroseulText">
+            <h3 className="smallText">{caroseuDialogue[currentPic].smallText}</h3>
+            <h3 className="bigText">{caroseuDialogue[currentPic].bigText}</h3>
+            <div className="btnSlide">
+              <button className="exploreBtn">Explore more</button>
+              <div>{caroseulSlide()}</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Main;
+  export default Main;
